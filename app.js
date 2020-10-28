1f88c9214f8d2304f3de1a4f0c039a155ed1a214
Diego Soria <diegosoriaarg@gmail.com>
2020-10-27 22:37:12 -0300

    otro cambio

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose');
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.9hwf3.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => console.log('Base de datos conectada'))
+.then(() => console.log('Base de datos conectada bien'))
 .catch(e => console.log(e))
 
 
